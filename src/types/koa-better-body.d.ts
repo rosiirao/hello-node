e08bb4a24a6448c1442a398b73732e7d6c022717
rosiirao <raoyu2009@yahoo.com>
2020-05-17 10:54:16 +0800

    remove unused file

--- src/types/koa-better-body.d.ts
+++
@@ -1,8 +0,0 @@
-import Koa from 'koa';
-
-declare module 'koa-better-body' {
-  interface bodyOptions {
-    handler?: Koa.Middleware;
-  }
-  export default function body(options?: bodyOptions): Koa.Middleware;
-}
